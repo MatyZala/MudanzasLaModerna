8ce063365d2c85971c027e31fca78de900b9c51b
MatyZala <mzx631@gmail.com>
2022-09-06 20:32:05 -0300

    Footer & Contact

--- mlm/src/container/Footer/Footer.jsx
+++ mlm/src/container/Footer/Footer.jsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
 
-import { FooterOverlay, Newsletter } from '../../components';
 import { images } from '../../constants';
 import './Footer.css';
 
 const Footer = () => (
   <div className="app__footer section__padding" id="login">
-    <FooterOverlay />
-    <Newsletter />
 
     <div className="app__footer-links">
       <div className="app__footer-links_contact">
-        <h1 className="app__footer-headtext">Contact Us</h1>
-        <p className="p__opensans">9 W 53rd St, New York, NY 10019, USA</p>
-        <p className="p__opensans">+1 212-344-1230</p>
-        <p className="p__opensans">+1 212-555-1230</p>
+        <h1 className="app__footer-headtext">Contacto</h1>
+        <p className="p__opensans">Av. Capdevila 900 | Córdoba - Argentina.</p>
+        <p className="p__opensans"> 473-1320</p>
+        <p className="p__opensans">+54 9 351-530-8384</p>
+        <p className="p__opensans">+54 9 351-226-3327</p>
       </div>
 
       <div className="app__footer-links_logo">
         <img src={images.gericht} alt="footer_logo" />
-        <p className="p__opensans">&quot;The best way to find yourself is to lose yourself in the service of others.&quot;</p>
-        <img src={images.spoon} className="spoon__img" style={{ marginTop: 15 }} />
+        <p className="p__opensans">&quot;Mudanzas La moderna <br /> seguridad y confianza.&quot;</p>
         <div className="app__footer-links_icons">
           <FiFacebook />
           <FiTwitter />
@@ -30,16 +27,16 @@ const Footer = () => (
       </div>
 
       <div className="app__footer-links_work">
-        <h1 className="app__footer-headtext">Working Hours</h1>
-        <p className="p__opensans">Monday-Friday:</p>
+        <h1 className="app__footer-headtext">Horarios de trabajo</h1>
+        <p className="p__opensans">lunes-viernes:</p>
         <p className="p__opensans">08:00 am - 12:00 am</p>
-        <p className="p__opensans">Saturday-Sunday:</p>
+        <p className="p__opensans">sabados-domingos:</p>
         <p className="p__opensans">07:00 am - 11:00 pm</p>
       </div>
     </div>
 
     <div className="footer__copyright">
-      <p className="p__opensans">2021 Gericht. All Rights reserved.</p>
+      <p className="p__opensans">2022 Mudanzas la moderna. All Rights reserved.</p>
     </div>
 
   </div>
