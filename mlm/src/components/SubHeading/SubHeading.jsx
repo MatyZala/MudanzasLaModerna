68194702b9209db0558fb7e8bc6f41175e328bf1
MatyZala <mzx631@gmail.com>
2022-09-13 20:42:17 -0300

    form

--- mlm/src/components/SubHeading/SubHeading.jsx
+++ mlm/src/components/SubHeading/SubHeading.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-/* import { images } from '../../constants'; */
-
 const SubHeading = ({ title }) => (
   <div style={{ marginBottom: '1rem' }}>
     <p className="p__cormorant">{title}</p>
--- mlm/src/container/FindUs/FindUs.jsx
+++ mlm/src/container/FindUs/FindUs.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './FindUs.css';
 
 import { SubHeading } from '../../components';
 
@@ -7,13 +8,17 @@ const FindUs = () => (
     <div className="app__wrapper_info">
       <SubHeading title="Contactanos" />
       <h1 className="headtext__cormorant" style={{ marginBottom: '3rem' }}>Mudanzas la moderna</h1>
-      <div className="app__wrapper-content">
-        <input type="text" placeholder="Nombre" />
-        <input type="email" placeholder="Email" />
-        <input type="number" placeholder="Telefono" />
-        <textarea type="text" placeholder="Consulta" />
+      <div className="container">
+        <div className="contact-box">
+          <div className="right">
+            <input type="text" className="field" placeholder="Nombre" />
+            <input type="email" className="field" placeholder="Email" />
+            <input type="tel" className="field" placeholder="Telefono" />
+            <textarea type="text" className="field area" placeholder="Consulta" />
+            <button type="button" className="btn" style={{ marginTop: '2rem' }}>Enviar</button>
+          </div>
+        </div>
       </div>
-      <button type="button" className="custom__button" style={{ marginTop: '2rem' }}>Enviar</button>
     </div>
 
   </div>
